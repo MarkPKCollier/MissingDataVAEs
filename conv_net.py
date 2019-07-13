ea7aaecf718478b3e1a87496dee1377bb4299c0d
Mark Collier <mcollier@hubspot.com>
2019-07-13 19:18:46 -0400

    progress save

--- conv_net.py
+++ conv_net.py
@@ -34,6 +34,7 @@ if args.save_imgs:
 # see: https://arxiv.org/pdf/1202.2745.pdf for conv architectures
 # Multi-column Deep Neural Networks for Image Classification
 if 'mnist' in args.dataset:
+    img_channels = 3
     data = tf.keras.datasets.mnist
 
     (x_train, y_train), (x_test, y_test) = data.load_data()
@@ -64,16 +65,17 @@ if 'mnist' in args.dataset:
 
     z_dim = 50
     input_units = x_train.shape[1] * x_train.shape[2]
-    encoder_layers = [(20, 3, 2), (40, 5, 2), 250]
+    encoder_layers = [(20, 5, 2), (40, 5, 2), 250]
     encoder_shapes = [(28, 28, 1), (14, 14, 20), (7, 7, 40)]
     encoder_shapes_pre_pool = encoder_shapes
     p_x_layers = [7*7*20,
                   ([-1, 7, 7, 20], 40, 5, 2, tf.nn.relu),
-                  (None, 20, 3, 2, tf.nn.relu)]
-    p_x_b_z_layers = [(None, 10, 3, 1, tf.nn.relu),
-                      (None, 5, 3, 1, tf.nn.relu),
+                  (None, 20, 5, 2, tf.nn.relu)]
+    p_x_b_z_layers = [(None, 10, 5, 1, tf.nn.relu),
+                      (None, 5, 5, 1, tf.nn.relu),
                       (None, 3 * args.k, 3, 1, None) if args.likelihood == 'logistic_mixture' else (None, 1, 3, 1, None)]
 elif 'svhn' in args.dataset:
+    img_channels = 3
     x_train, x_test = chainer.datasets.get_svhn(withlabel=False, scale=255.0)
 
     x_train = np.concatenate((np.expand_dims(x_train[:, 0, :, :], -1),
@@ -88,18 +90,20 @@ elif 'svhn' in args.dataset:
 
     x_train, x_valid = train_test_split(x_train, train_size=63257, test_size=10000)
 
-    z_dim = 100
+    z_dim = 150
     input_units = x_train.shape[1] * x_train.shape[2]
-    encoder_layers = [(20, 3, 2), (40, 5, 2), 250]
-    encoder_shapes = [(32, 32, 3), (16, 16, 20), (8, 8, 40)]
+    encoder_layers = [(40, 5, 2), (60, 5, 2), (60, 5, 2), 300]
+    encoder_shapes = [(32, 32, 3), (16, 16, 40), (8, 8, 60), (4, 4, 60)]
     encoder_shapes_pre_pool = encoder_shapes
-    p_x_layers = [8*8*20,
-                  ([-1, 8, 8, 20], 40, 5, 2, tf.nn.relu),
-                  (None, 20, 3, 2, tf.nn.relu)]
-    p_x_b_z_layers = [(None, 10, 3, 1, tf.nn.relu),
-                      (None, 5, 3, 1, tf.nn.relu),
+    p_x_layers = [4*4*60,
+                  ([-1, 4, 4, 60], 60, 5, 2, tf.nn.relu),
+                  (None, 60, 5, 2, tf.nn.relu),
+                  (None, 40, 5, 2, tf.nn.relu)]
+    p_x_b_z_layers = [(None, 30, 5, 1, tf.nn.relu),
+                      (None, 30, 5, 1, tf.nn.relu),
                       (None, 9 * args.k, 3, 1, None) if args.likelihood == 'logistic_mixture' else (None, 3, 3, 1, None)]
 elif args.dataset == 'cifar10':
+    img_channels = 3
     data = tf.keras.datasets.cifar10
 
     (x_train, y_train), (x_test, y_test) = data.load_data()
@@ -222,25 +226,42 @@ def decoder(z, b=None):
     for i, layer in enumerate(p_x_b_z_layers):
         _, filters, kernel_size, strides, activation = layer
 
-        net = tf.layers.conv2d_transpose(net, filters, kernel_size,
-                                         strides=strides, activation=activation,
-                                         padding='SAME')
+        if i == 0:
+            if b is not None:
+                in_channels = p_x_layers[-1][1] + 1
+            else:
+                in_channels = p_x_layers[-1][1]
+        else:
+            in_channels = p_x_b_z_layers[i-1][1]
+
+        w = tf.get_variable('layer_{0}_w'.format(i),
+                            shape=(kernel_size, kernel_size, in_channels, filters),
+                            initializer=tf.contrib.layers.xavier_initializer())
+
+        net = tf.nn.conv2d(net, w, strides=(1, strides, strides, 1), padding='SAME')
 
         bias = tf.get_variable('p_x_b_z_layer_{0}_bias'.format(i),
                                shape=p_x_b_z_layers[i][1],
                                initializer=tf.initializers.zeros())
 
         net = tf.nn.bias_add(net, bias)
+        
+        if activation is not None:
+            net = activation(net)
 
-        if args.likelihood == 'logistic_mixture':
-            assert 'mnist' not in args.dataset
-            mean_logit = net[:, :, :, 0:3*args.k]
-            scale_logit = net[:, :, :, 3*args.k:6*args.k]
-            pi_logit = net[:, :, :, 6*args.k:]
-        else:
-            mean_logit = net
-            scale_logit = None
-            pi_logit = None
+    if args.likelihood == 'logistic_mixture':
+        assert 'mnist' not in args.dataset
+        mean_logit = []
+        scale_logit = []
+        pi_logit = []
+        for i in range(img_channels):
+            mean_logit.append(net[:, :, :, i*args.k:(i+1)*args.k])
+            scale_logit.append(net[:, :, :, (img_channels*args.k + i*args.k):(img_channels*args.k + (i+1)*args.k)])
+            pi_logit.append(net[:, :, :, (2*img_channels*args.k + i*args.k):(2*img_channels*args.k + (i+1)*args.k)])
+    else:
+        mean_logit = net
+        scale_logit = None
+        pi_logit = None
 
     return mean_logit, scale_logit, pi_logit
 
@@ -290,35 +311,25 @@ def model(features, labels, mode, params):
     kl = tf.reduce_mean(tf.reduce_sum(tf.distributions.kl_divergence(q_z, p_z), axis=1))
 
     def log_probs(z_sample):
-        if args.dataset == 'binary_mnist' or args.dataset == 'mnist':
-            x_logits, scale_logit, pi_logit = decoder(z_sample,
-                                                      b=b if '_decoder_b' in params['model_type'] else None)
-            x_logits = tf.reshape(x_logits, [-1, img_dim, img_dim])
+        x_logits, scale_logit, pi_logit = decoder(z_sample,
+                                                  b=b if '_decoder_b' in params['model_type'] else None)
+        if args.dataset == 'mnist':
+            x_logits = tf.squeeze(x_logits, axis=-1)
+        if args.likelihood == 'bernoulli':
             x_pred = tf.nn.sigmoid(x_logits)
-        elif args.dataset in ('cifar10', 'svhn'):
-            x_logits, scale_logit, pi_logit = decoder(z_sample,
-                                                      b=b if '_decoder_b' in params['model_type'] else None)
-            
-            if args.likelihood == 'logistic_mixture':
-                scale = tf.exp(scale_logit) + 1e-2
+        if args.likelihood == 'logistic_mixture':
+            scale = [tf.exp(scale_logit[i]) + 1e-2
+                     for i in range(img_channels)]
 
-                # pi = tf.concat([tf.nn.softmax(pi_logit[:, :, :, i*3:(i+1)*3], axis=3)
-                #                for i in range(args.k)], axis=3)
+            pi = [tf.nn.softmax(pi_logit[i], axis=3)
+                  for i in range(img_channels)]
 
-                if args.k == 1:
-                    x_pred = x_logits
-                else:
-                    x_pred = tf.concat([tf.expand_dims(tf.reduce_sum(tf.gather(pi * x_logits,
-                                                                  [i * 3 + j for i in range(args.k)],
-                                                                  axis=3), axis=3), axis=3)
-                                       for j in range(3)], axis=3)
-
-                    # x_pred = tf.reduce_sum(tf.stack([pi[:, :, :, i*3:(i+1)*3] * x_logits[:, :, :, i*3:(i+1)*3]
-                    #                                 for i in range(args.k)], axis=3),
-                    #                        axis=4)
+            if args.k == 1:
+                x_pred = tf.concat([tf.expand_dims(tf.reduce_sum(x_logits[i], axis=3), axis=3)
+                                    for i in range(img_channels)], axis=3)
             else:
-                x_logits = tf.reshape(x_logits, [-1, img_dim, img_dim, 3])
-                x_pred = tf.nn.sigmoid(x_logits)
+                x_pred = tf.concat([tf.expand_dims(tf.reduce_sum(pi[i] * x_logits[i], axis=3), axis=3)
+                                    for i in range(img_channels)], axis=3)
 
         if args.likelihood == 'bernoulli':
             # valid for even real valued MNIST: http://ruishu.io/2018/03/19/bernoulli-vae/
@@ -329,33 +340,36 @@ def model(features, labels, mode, params):
             imputation_log_prob = tf.reduce_mean(tf.reduce_sum((1.0 - b) * log_prob_full,
                                                  axis=[2,1] if 'mnist' in args.dataset else [3,2,1]))
         elif args.likelihood == 'logistic_mixture':
-            x_expanded = tf.concat([x for _ in range(args.k)], axis=3)
-            centered_x = x_expanded - x_logits
-            upper_in = (centered_x + (1.0/255.0))/scale
-            upper_cdf = tf.nn.sigmoid(upper_in)
-            lower_in = (centered_x - (1.0/255.0))/scale
-            lower_cdf = tf.nn.sigmoid(lower_in)
-            cdf_delta = upper_cdf - lower_cdf
-            
-            mid_in = centered_x/scale
-            log_pdf_mid = mid_in - scale_logit - 2.0 * tf.nn.softplus(mid_in)
-            log_cdf_plus = upper_in - tf.nn.softplus(upper_in)
-            log_one_minus_cdf_min = -tf.nn.softplus(lower_in)
-            
-            log_prob_comp = tf.where(x_expanded < -0.999,
-                                     log_cdf_plus,
-                                     tf.where(x_expanded > 0.999,
-                                              log_one_minus_cdf_min,
-                                              tf.where(cdf_delta > 1e-5,
-                                                       tf.log(tf.maximum(cdf_delta, 1e-12)),
-                                                       log_pdf_mid - np.log(127.5))))
+            log_prob_full = []
+            for i in range(img_channels):
+                x_expanded = tf.concat([tf.expand_dims(x[:, :, :, i], axis=3)
+                                        for _ in range(args.k)], axis=3)
+                centered_x = x_expanded - x_logits[i]
+                upper_in = (centered_x + (1.0/255.0))/scale[i]
+                upper_cdf = tf.nn.sigmoid(upper_in)
+                lower_in = (centered_x - (1.0/255.0))/scale[i]
+                lower_cdf = tf.nn.sigmoid(lower_in)
+                cdf_delta = upper_cdf - lower_cdf
+                
+                mid_in = centered_x/scale[i]
+                log_pdf_mid = mid_in - scale_logit[i] - 2.0 * tf.nn.softplus(mid_in)
+                log_cdf_plus = upper_in - tf.nn.softplus(upper_in)
+                log_one_minus_cdf_min = -tf.nn.softplus(lower_in)
+                
+                log_prob_comp = tf.where(x_expanded < -0.999,
+                                         log_cdf_plus,
+                                         tf.where(x_expanded > 0.999,
+                                                  log_one_minus_cdf_min,
+                                                  tf.where(cdf_delta > 1e-5,
+                                                           tf.log(tf.maximum(cdf_delta, 1e-12)),
+                                                           log_pdf_mid - np.log(127.5))))
+                if args.k > 1:
+                    log_prob_comp += tf.log(pi[i])
 
-            log_prob_comp += tf.log(pi)
+                log_prob_full.append(log_sum_exp(log_prob_comp))
 
-            log_prob_full = tf.concat([tf.expand_dims(log_sum_exp(tf.gather(log_prob_comp,
-                                                                  [i * 3 + j for i in range(args.k)],
-                                                                  axis=3)), axis=3)
-                                       for j in range(3)], axis=3)
+            log_prob_full = tf.concat([tf.expand_dims(log_prob_full[i], axis=3)
+                                       for i in range(img_channels)], axis=3)
 
             log_prob = tf.reduce_mean(
                 tf.reduce_sum(b * log_prob_full,
@@ -550,13 +564,11 @@ for missingness_type, p in [('independent', 0.5)]:
     print 'Average pixel intensity:', np.mean(mu_hat)
     print 'Average pixel variance:', np.mean(sigma_hat)
     
-    # for model_type, title in [('VAE', 'Zero imputation'),
-    #                    ('VAE_mean_imp', 'Mean imputation'),
-    #                    ('VAE_ind', 'Zero imputation w/ indicator variables'),
-    #                    ('VAE_ind_mean_imp', 'Mean imputation w/ encoder indicator variables'),
-    #                    ('VAE_ind_decoder_b', 'Zero imputation w/ encoder + decoder indicator variables'),
-    #                    ('VAE_ind_mean_imp_decoder_b', 'Mean imputation w/ encoder + decoder indicator variables')]:
-    for model_type, title in [('VAE_ind_decoder_b', 'Zero imputation w/ encoder + decoder indicator variables')]:
+    for model_type, title in [('VAE', 'Zero imputation'),
+                       ('VAE_mean_imp', 'Mean imputation'),
+                       ('VAE_ind', 'Zero imputation w/ indicator variables'),
+                       ('VAE_ind_decoder_b', 'Zero imputation w/ encoder + decoder indicator variables')]:
+    # for model_type, title in [('VAE_ind_decoder_b', 'Zero imputation w/ encoder + decoder indicator variables')]:
         if '_mean_imp' in model_type:
             x_train_input = b_train * x_train + (1.0 - b_train) * mu_hat
             x_valid_input = b_valid * x_valid + (1.0 - b_valid) * mu_hat
@@ -586,7 +598,7 @@ for missingness_type, p in [('independent', 0.5)]:
 
                     total += 1
             fig.suptitle(title, fontsize=35, verticalalignment='center', y=0.92)
-            plt.savefig('{0}_{1}.png'.format(args.dataset), bbox_inches='tight')
+            plt.savefig('{0}_{1}.png'.format(args.dataset, model_type), bbox_inches='tight')
             plt.close()
         elif args.save_imgs and args.dataset in ('cifar10', 'svhn'):
             def draw_digit(data, row, col, n):
